e4b8ff951839aa80942638bd8a9c3e531629f360
Aniket Kumar <aniketkumar049@gmail.com>
2020-04-22 00:35:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ cd gatsby-site
 npm build
 cd ../cra-site
 npm build
-rm -rf ../gatsby-site/public/cra
+# rm -rf ../gatsby-site/public/cra
 mv build ../gatsby-site/public/cra
 cd ../gatsby-site
 cp ../_redirects public/
-netlify deploy --dir=public --prod
+# netlify deploy --dir=public --prod
